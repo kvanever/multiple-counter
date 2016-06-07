15da2f1b4be7a6705fc8f3851e6a3844c8795e23
Kevin VanEvery and Leah Nelson <kevin@>
2016-06-07 08:33:58 -0700

    add validation to form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,14 @@ $(function() {
     event.preventDefault();
     var total = parseInt($("input#number1").val());
     var multiple = parseInt($("input#number2").val());
-    debugger;
+    if (total >= 0 && multiple > 0 && multiple < total) {
       for (i=1; i <= (total/multiple); i++) {
         $(".output ul").append("<li>" + (i * multiple) + "</li>");
     };
+    }
+    else {
+      alert("Please use positive integers and make sure number 2 is less than number 1");
+    }
+
   });
 });
