efe1ffdf71bc77e9d0aa3056738d44e906e15d63
Kevin VanEvery and Leah Nelson <kevin@>
2016-06-07 08:25:13 -0700

    create html markup and js to count multiples

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-/*This file is for your custom js.  All yours*/
+$(function() {
+  $("form#number-input").submit(function(event) {
+    event.preventDefault();
+    var total = parseInt($("input#number1").val());
+    var multiple = parseInt($("input#number2").val());
+    debugger;
+      for (i=1; i <= (total/multiple); i++) {
+        $(".output ul").append("<li>" + (i * multiple) + "</li>");
+    };
+  });
+});
